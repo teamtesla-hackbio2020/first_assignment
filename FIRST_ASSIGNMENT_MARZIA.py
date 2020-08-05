25d6edad37a8c459abe07450a0b947aedff24e51
Marz2720 <69089034+Marz2720@users.noreply.github.com>
2020-08-06 01:13:20 +0600

    Update FIRST_ASSIGNMENT_MARZIA.py

--- FIRST_ASSIGNMENT_MARZIA.py
+++ FIRST_ASSIGNMENT_MARZIA.py
@@ -2,7 +2,7 @@ My_name = "Humaira Marzia Alam"
 Email = "alamhumaira1@gmail.com"
 Language_used = "Python 3"
 My_biostack = "Genomics"
-Slack_username = @Marz2720
+Slack_username = "@Marzia"
 
 #print("Name:%s"%My_name+"Email:%s"%Email+"Language used to code:%s"%Language_used +"Biostack:%s"% My_biostack +"Slack username: %s"%Slack_username)
 print ("Name:%s \nEmail:%s\nlanguage used to code:%s\nBiostack:%s\nslack username:%s" %(My_name,Email,Language_used,My_biostack,Slacck_username)) 
