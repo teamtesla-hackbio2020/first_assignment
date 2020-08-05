3697b00b015b1f7c6433132525691de74c1ec9b8
teamtesla-hackbio2020 <69094446+teamtesla-hackbio2020@users.noreply.github.com>
2020-08-05 13:58:42 +0000

    Update teamteslabashscript.sh

--- teamteslabashscript.sh
+++ teamteslabashscript.sh
@@ -2,4 +2,4 @@
 
 xargs -n1 git clone <teamteslarepo.txt
 
-tr -s " " </first_assignment/teslamembersdetails.txt | sed 's/ /,/g' > members.csv
+tr -s " " <./first_assignment/teslamembersdetails.txt | sed 's/ /,/g' > members.csv
