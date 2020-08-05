cfb3a95c9e353cd899d15f5275c4cc1dc12cb220
Marz2720 <69089034+Marz2720@users.noreply.github.com>
2020-08-05 22:08:18 +0600

    First Assignment Marzia
    Submitting my first assignment using python as the programming language.

---
+++ FIRST_ASSIGNMENT_MARZIA.py
@@ -0,0 +1,8 @@
+My_name = "Humaira Marzia Alam"
+Email = "alamhumaira1@gmail.com"
+Language_used = "Python 3"
+My_biostack = "Genomics"
+Slack_username = @Marz2720
+
+#print("Name:%s"%My_name+"Email:%s"%Email+"Language used to code:%s"%Language_used +"Biostack:%s"% My_biostack +"Slack username: %s"%Slack_username)
+print ("Name:%s \nEmail:%s\nlanguage used to code:%s\nBiostack:%s\nslack username:%s" %(My_name,Email,Language_used,My_biostack,Slacck_username)) 
