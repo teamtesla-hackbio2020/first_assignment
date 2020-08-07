50b9759268bc469be307341e2f1817773111c385
teamtesla-hackbio2020 <69094446+teamtesla-hackbio2020@users.noreply.github.com>
2020-08-07 11:12:55 +0100

    Update bash script

--- teamteslabashscript.sh
+++ teamteslabashscript.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
-xargs -n1 git clone <teamteslarepo.txt
+git clone https://github.com/teamtesla-hackbio2020/team-tesla.git
 
-tr -s " " <./first_assignment/teslamembersdetails.txt | sed 's/ /,/g' > teamteslamembers.csv
+cd ./team-tesla
+
+for file in $(ls);
+
+do
+	if [[ $file == *.py ]];
+	then
+		python $file >> ../tesla.csv
+	elif [[ $file == *.rb ]];
+	then
+		ruby $file >> ../tesla.csv
+	elif [[ $file == *.c || $file == *.cpp ]];
+	then
+		g++ $file 
+		./a.out >> ../tesla.csv
+		rm a.out
+	
+	elif [[ $file == *.cs ]];
+	then
+		csc $file 
+		./a.out >> ../tesla.csv
+		rm a.out
+
+	elif [[ $file == *.pl ]];
+	then
+		perl $file >> ../tesla.csv
+	elif [[ $file == *.js ]];
+	then
+		node $file >> ../tesla.csv
+
+	elif [[ $file == *.dart ]];
+	then
+		dart $file >> ../tesla.csv
+
+	elif [[ $file == *.java ]];
+        then
+                javac $file
+		fname=`echo $file | cut -d\. -f1`
+		java $fname >> ../tesla.csv
+	fi
+done
