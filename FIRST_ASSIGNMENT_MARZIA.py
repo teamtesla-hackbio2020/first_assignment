60304e555f24218a4f4dec4168988e67a3b315f8
Jabale Rahmat <69088905+Taurpio@users.noreply.github.com>
2020-08-10 00:59:31 +0600

    Updated_Marzia
    Error has been fixed.

--- FIRST_ASSIGNMENT_MARZIA.py
+++ FIRST_ASSIGNMENT_MARZIA.py
@@ -5,4 +5,4 @@ My_biostack = "Genomics"
 Slack_username = "@Marzia"
 
 #print("Name:%s"%My_name+"Email:%s"%Email+"Language used to code:%s"%Language_used +"Biostack:%s"% My_biostack +"Slack username: %s"%Slack_username)
-print ("Name:%s \nEmail:%s\nlanguage used to code:%s\nBiostack:%s\nslack username:%s" %(My_name,Email,Language_used,My_biostack,Slacck_username)) 
+print ("Name:%s \nEmail:%s\nlanguage used to code:%s\nBiostack:%s\nslack username:%s" %(My_name,Email,Language_used,My_biostack,Slack_username)) 
